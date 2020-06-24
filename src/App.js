1977e683d92950d75aade174eee67fc351ae032c
Amos Welike <amoswelike@gmail.com>
2020-06-24 12:17:10 +0300

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [price, setPrice] = useState('')
+  const [name, setName] = useState('')
+  const handlePriceChange = (e) => {
+    e.preventDefault();
+    setPrice(e.target.value);
+  }
+
+  const handleNameChange = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    fetch('http://127.0.0.1:5000/sendmail')
+      .then(res=>res.json())
+      .then(data => console.log(data))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+    <div className="card-body">
+      <div className="form-group">
+        <label>Name:</label>
+        <input type="email" name="name" onChange={handleNameChange} value={name} className="form-control" id="email" />
+      </div>
+      <div className="form-group">
+        <label>Price:</label>
+        <input type="text" name='price' onChange={handlePriceChange} value={price} className="form-control" id="pwd" />
+      </div>
+      <button type="submit" className="btn btn-default" onClick={handleSubmit}>Submit</button>
+    </div>
     </div>
   );
 }
